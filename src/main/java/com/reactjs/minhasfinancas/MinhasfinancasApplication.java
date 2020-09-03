de0597ea32c31d442ab541780d2f0fe51787d14d
Anderson Bonavides <ambonavides@gmail.com>
2020-09-03 09:14:33 -0300

    ldfkja

--- src/main/java/com/reactjs/minhasfinancas/MinhasfinancasApplication.java
+++ src/main/java/com/reactjs/minhasfinancas/MinhasfinancasApplication.java
@@ -2,9 +2,16 @@ package com.reactjs.minhasfinancas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class MinhasfinancasApplication {
+public class MinhasfinancasApplication implements WebMvcConfigurer{
+	
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MinhasfinancasApplication.class, args);
--- src/main/java/com/reactjs/minhasfinancas/api/dto/UsuarioDTO.java
+++ src/main/java/com/reactjs/minhasfinancas/api/dto/UsuarioDTO.java
@@ -1,10 +1,16 @@
 package com.reactjs.minhasfinancas.api.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Builder
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UsuarioDTO {
 	
 	private String nome;
